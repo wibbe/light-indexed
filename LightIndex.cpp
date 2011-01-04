27f9f97846d2178471084fa9009821ea4943d8de
Daniel Wiberg <daniel.wiberg@oryx.se>
2011-01-04 14:20:18 +0100

    Initial commit.

---
+++ LightIndex.cpp
@@ -0,0 +1,11 @@
+
+#include "LightIndex.h"
+
+LightIndex::LightIndex()
+{
+}
+
+LightIndex::~LightIndex()
+{
+}
+
---
+++ LightIndex.h
@@ -0,0 +1,13 @@
+
+#ifndef LIGHT_INDEX_H
+#define LIGHT_INDEX_H
+
+class LightIndex
+{
+  public:
+    LightIndex();
+    ~LightIndex();
+};
+
+#endif
+
