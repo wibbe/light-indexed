fc5e2ed6c99805ddd2dadbfa8cdfb3580586ff14
Daniel Wiberg <daniel.wiberg@oryx.se>
2011-01-04 14:41:31 +0100

    Made it possible to create lights, and change some of their properties.

--- LightIndex.cpp
+++ LightIndex.cpp
@@ -1,11 +1,55 @@
 
 #include "LightIndex.h"
+#include <casser>
 
-LightIndex::LightIndex()
+namespace lidr
 {
-}
 
-LightIndex::~LightIndex()
-{
+  LightIndex::LightIndex()
+  {
+  }
+  
+  LightIndex::~LightIndex()
+  {
+  }
+
+  LightId LightIndex::createLight()
+  {
+    if (m_lights.size() >= 255)
+      return -1;
+
+    Light light = { 0.0f, 0.0f, 0.0f, 100.0f, 255, 255, 255 };
+    m_lights.push_back(light);
+    return m_lights.size();
+  }
+
+  void LightIndex::setPosition(LightId id, float x, float y, float z)
+  {
+    assert(id > 0 && id <= m_lights.size());
+
+    Light & light = m_lights[id];
+    light.x = x;
+    light.y = y;
+    light.z = z;
+  }
+
+  void LightIndex::setAttenuation(LightId id, float attenuation)
+  {
+    assert(id > 0 && id <= m_lights.size());
+
+    Light & light = m_lights[id];
+    light.attenuation = attenuation;
+  }
+
+  void LightIndex::setColor(LightId id, unsigned char r, unsigned char g, unsigned char b)
+  {
+    assert(id > 0 && id <= m_lights.size());
+
+    Light & light = m_lights[id];
+    light.r = r;
+    light.g = g;
+    light.b = b;
+  }
+
 }
 
--- LightIndex.h
+++ LightIndex.h
@@ -2,12 +2,37 @@
 #ifndef LIGHT_INDEX_H
 #define LIGHT_INDEX_H
 
-class LightIndex
+#include <vector>
+
+namespace lidr
 {
-  public:
-    LightIndex();
-    ~LightIndex();
-};
+  typedef unsigned int LightId;
+
+  class LightIndex
+  {
+    private:
+      struct Light
+      {
+        float x, y, z;
+        float attenuation;
+        unsigned char r, g, b;
+      };
+
+    public:
+      LightIndex();
+      ~LightIndex();
+
+      LightId createLight();
+
+      void setPosition(LightId id, float x, float y, float z);
+      void setAttenuation(LightId id, float attenuation);
+      void setColor(LightId id, unsigned char r, unsigned char g, unsigned char b);
+  
+    private:
+      std::vector<Light> m_lights;
+  };
+
+}
 
 #endif
 
